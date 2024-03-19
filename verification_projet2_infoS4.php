cc53c3b0ccd8a0a60026c387a0f3f8765eecd5ef
curdette <150437994+curdette@users.noreply.github.com>
2024-03-19 12:39:24 +0100

    Update verification_projet2_infoS4.php
    h'ai rajouter mail et id_membre, jsp si les dernière vérif fonctionne faudra teste

--- verification_projet2_infoS4.php
+++ verification_projet2_infoS4.php
@@ -27,11 +27,11 @@ if(isset($_POST['username']) && isset($_POST['password']))
  if($count!=0) // nom d'utilisateur et mot de passe correctes
  {
  $_SESSION['username'] = $username;
- header('Location: principale.php');
+ header('Location: principale.php');//utilisateur ou mot de passe incorrect ici jsp si ça va marcher pour id_membre et mail ?? 
  }
  else
  {
- header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
+ header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect ici jsp si ça va marcher pour id_membre et mail ?? 
  }
  }
  else
