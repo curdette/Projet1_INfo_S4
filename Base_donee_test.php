d85a54d16c15787f7bf6dc950b8bfa9da8332fcb
shezza75 <150438238+shezza75@users.noreply.github.com>
2024-03-19 11:24:30 +0100

    Base_donee_test.php

--- Base_donee_test.php
+++ Base_donee_test.php
@@ -1,17 +1,33 @@
 
 <?php
-$serveur = "localhost"; 
-$utilisateur = "root"; 
-$mdp = ""; 
-$bdd = "base_test";
-$connexion = mysqli_connect($serveur, $utilisateur, $mdp, $bdd);
+// Informations de connexion à la base de données
+$serveur = "localhost";
+$utilisateur = "root";
+$mot_de_passe = "";
+$base_de_donnees = "projet_info";
 
-if (!$connexion) {
-    die("La connexion a échoué : " . mysqli_connect_error());
-}
-else{
-    echo"on est la" ;
-}
+// Créer une connexion
+$connexion = new mysqli($serveur, $utilisateur, $mot_de_passe, $base_de_donnees);
 
+// Vérifier la connexion
+if ($connexion->connect_error) {
+    die("Échec de la connexion : " . $connexion->connect_error);}
+
+
+// Selectionne tous les enregistrements de livre
+$query = "SELECT * FROM livre";
+$result = $connexion->query($query); // [lengths] -> nb_colonnes et [num_rows] -> nb_lignes
+if ($result) {
+    // La requête a réussi, vous pouvez traiter les données ici
+    echo "ça a marché <br>" ;
+    while ($row = $result->fetch_assoc()) {
+        // Traitement de chaque ligne de résultat
+        echo "ISBN: " . $row["ISBN"] . ", Titre: " . $row["Titre"] . "<br>";
+    }
+} else {
+    // La requête a échoué
+    echo "Erreur lors de l'exécution de la requête : " . $connexion->error;
+}
 
 ?>
+
